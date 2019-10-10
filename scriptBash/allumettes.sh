8cc77dc58832ad8419a9249e551807e3f72082ce
Ackermann <zayyfix@gmail.com>
2019-10-10 16:05:53 +0200

    script bonus

--- scriptBash/allumettes.sh
+++ scriptBash/allumettes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly MIN=1
+readonly MAX=3
 nbAllumettes=21
 
 echo "
@@ -15,20 +17,86 @@ read -r debutPartie
 
 case $debutPartie in
     J)
-        echo "J"
+		allumettesTirerJoueur=0
+		allumettesTirerMachine=0
         while [ $nbAllumettes != "0" ]
         do
-        echo "il reste $nbAllumettes allumette(s)"
-	echo "Nb allumettes (1, 2 ou 3) ?" #TODO BLOCK ALLUMETTES > 3 & ==0
-            read -r nbAllumettesChoisis
-            let "nbAllumettes = nbAllumettes - nbAllumettesChoisis"
-            allumetteAleatoire=$((1 + RANDOM % 3))
-            echo "La machine a choisie $allumetteAleatoire allumettes."
-            let "nbAllumettes = nbAllumettes - nbAllumettesChoisis"
+        echo "Il reste $nbAllumettes allumette(s)"
+		echo "Nb allumettes (1, 2 ou 3) ?"
+        read -r nbAllumettesChoisis
+		let "test = nbAllumettes - nbAllumettesChoisis"
+		while [ -z $nbAllumettesChoisis ] || [ $nbAllumettesChoisis -gt 3 ] || [ $nbAllumettesChoisis -eq 0 ] || [ $nbAllumettesChoisis -gt $nbAllumettes ] || [ $test -lt 0 ] 
+		do
+			echo "Impossible"
+			read -r nbAllumettesChoisis
+		done
+		let "++allumettesTirerJoueur"
+        let "nbAllumettes = nbAllumettes - nbAllumettesChoisis"
+		if [ $nbAllumettes -eq 0 ]
+		then
+			break;
+		fi
+        allumetteAleatoire=$((MIN + RANDOM % MAX))
+		while [ $allumetteAleatoire -gt $nbAllumettes ]
+		do
+			allumetteAleatoire=$((MIN + RANDOM % MAX))
+		done
+		let "++allumettesTirerMachine"
+        echo "La machine a choisie $allumetteAleatoire allumettes."
+        let "nbAllumettes = nbAllumettes - allumetteAleatoire"
         done
+		if [ $allumettesTirerJoueur -gt $allumettesTirerMachine ]
+		then
+			echo "La machine a gagnée !"
+			exit 0
+		else
+			echo "Le joueur a gagné !"
+			exit 0
+		fi
         ;;
     M)
-        echo "M"
+		allumettesTirerJoueur=0
+		allumettesTirerMachine=0
+        while [ $nbAllumettes != "0" ]
+        do
+			allumetteAleatoire=$((MIN + RANDOM % MAX))
+			while [ $allumetteAleatoire -gt $nbAllumettes ]
+			do
+				allumetteAleatoire=$((MIN + RANDOM % MAX))
+			done
+			let "++allumettesTirerMachine"
+        	echo "La machine a choisie $allumetteAleatoire allumette(s)."
+        	let "nbAllumettes = nbAllumettes - allumetteAleatoire"
+			if [ $nbAllumettes -eq 0 ]
+			then
+				break;
+			fi
+        	echo "Il reste $nbAllumettes allumette(s)"
+			echo "Nb allumettes (1, 2 ou 3) ?"
+        	read -r nbAllumettesChoisis
+			let "test = nbAllumettes - nbAllumettesChoisis"
+			while [ -z $nbAllumettesChoisis ] || [ $nbAllumettesChoisis -gt 3 ] || [ $nbAllumettesChoisis -eq 0 ] || [ $nbAllumettesChoisis -gt $nbAllumettes ] || [ $test -lt 0 ] 
+			do
+				echo "Impossible"
+				read -r nbAllumettesChoisis
+			done
+        	let "nbAllumettes = nbAllumettes - nbAllumettesChoisis"
+			if [ $nbAllumettes -eq 0 ]
+			then
+				break;
+			fi
+			let "++allumettesTirerJoueur"
+        done
+		echo $allumettesTirerJoueur
+		echo $allumettesTirerMachine
+		if [ $allumettesTirerJoueur - $allumettesTirerMachine ]
+		then
+			echo "La machine a gagnée !"
+			exit 0
+		else
+			echo "Le joueur a gagné !"
+			exit 0
+		fi
         ;;
     T)
         echo "T"
--- scriptBash/infouser.sh
+++ scriptBash/infouser.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "L'utilisateur $1 s'est connecté / fois."
+echo -n "L'utilisateur $1 s'est connecté "
+echo -n $(last $1 | sed '/^$\|wtmp/d' | wc -l)
+echo " fois."
 echo "Ses 3 dernières connexions sont: "
 last $1 | cut -c40-
 
