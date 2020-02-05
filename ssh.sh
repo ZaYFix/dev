c577fefd96628a23bc2f963dd88736448afcbe7a
Ackermann <zayyfix@gmail.com>
2020-02-05 14:18:45 +0100

    rm ssh & push sh

--- ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh-keygen -o # Generate the key
-ssh-add ~/.ssh/id_rsa # Add the key in the ssh agent
-gedit ~/.ssh/id_rsa.pub # Open the public key, copy everything in it
-firefox "https://github.com/settings/ssh/new" # Paste everything here
